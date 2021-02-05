02b63add37545054c0718d3cb016a6c15f9d8270
hutinoatari <hutinoatari@gmail.com>
2021-02-05 21:51:47 +0900

    作品ページ追加

---
+++ src/pages/works.tsx
@@ -0,0 +1,17 @@
+import{ FC } from "react";
+
+const Works: FC<{}> = () => {
+    return (
+        <>
+        <h2>WORKS</h2>
+        <ol reversed>
+            <li>Space Wandering</li>
+            <li>食神セタップ</li>
+            <li>流動者</li>
+            <li>期限ギリギリスト</li>
+        </ol>
+        </>
+    );
+}
+
+export default Works;
